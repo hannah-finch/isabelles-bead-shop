5057c47409037278581c2cdfd6c105719200ea34
Kalink52 <164951917+Kalink52@users.noreply.github.com>
2024-09-12 13:35:38 -0400

    added server, and added to package.json
    i got enough of the server to run, and added to main package json to get server running.

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
