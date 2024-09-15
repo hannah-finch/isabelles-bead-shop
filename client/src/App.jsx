72459417d43812837ceba546c9dba9701f43a80a
Kalink52 <164951917+Kalink52@users.noreply.github.com>
2024-09-15 16:03:35 -0400

    app.jsx page set up with apollo client
    still have to add auth on client side, and still get user to log in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,37 @@
 import { Outlet } from "react-router-dom";
 
-function App() {
-  // const [count, setCount] = useState(0);
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+function App() {
   return (
     <>
-      <Outlet />
+      <ApolloProvider client={client}>
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
