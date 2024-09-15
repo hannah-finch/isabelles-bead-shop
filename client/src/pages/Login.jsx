82d8ebe357b3b08880c99b4b671b4155d15e7a57
hannahschwen <hannahschwen@gmail.com>
2024-09-15 15:20:29 -0400

    start login form structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,54 @@
+import { useState, useRef } from 'react';
+
+
+const LoginForm = (() => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+
+
+
+  
+  const handleFormSubmit = (e) => {
+  
+  }
+
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+    <input
+      value={username}
+      name="username"
+      // onChange={handleInputChange}
+      // onBlur={handleBlur}
+      type="text"
+      placeholder="username"
+      required
+    />
+    <label htmlFor="username">{usernameMessage}</label><br></br>
+    <input
+      value={password}
+      name="password"
+      // onChange={handleInputChange}
+      // onBlur={handleBlur}
+      type="password"
+      placeholder="password"
+      required
+    />
+    <label htmlFor="password">{passwordMessage}</label><br></br>
+    <button className="submit-btn" type="submit">
+      Submit
+    </button>
+  </form>
+  )
+})
+
+
+
+
+
 function LoginPage() {
   return (
     <>
