e0edad96ef4e3b260f13c74ea106f9f7cdd19250
hannahschwen <hannahschwen@gmail.com>
2024-09-15 16:03:10 -0400

    header and footer connect to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
 
+import Header from './components/header'
+import Footer from './components/footer'
+
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <Outlet />
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <h1>Header</h1>
+  )
+}
+
+
+export default Header
