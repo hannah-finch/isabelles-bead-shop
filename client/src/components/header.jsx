97e4829892b47d2f1717ffde5f79b780be5af9e2
hannahschwen <hannahschwen@gmail.com>
2024-09-15 16:17:31 -0400

    structure navbar

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,27 @@
+import { NavLink } from 'react-router-dom';
+
 function Header() {
   return (
-    <h1>Header</h1>
+    <header>
+      <nav>
+        {/* these page routes aren't made yet */}
+        <NavLink to={'/about'} className={({ isActive }) => (isActive ? "active-link" : "")}>
+          about
+        </NavLink>
+        <NavLink to={'/account'} className={({ isActive }) => (isActive ? "active-link" : "")}>
+          account
+        </NavLink>
+        <div className="vertical-line"></div>
+        <NavLink to={'/cart'} className={({ isActive }) => (isActive ? "active-link" : "")}>
+          cart
+        </NavLink>
+      </nav>
+      <div className="logo-container">
+        <h1>Isabelle&apos;s Bead Shop</h1>
+        <img></img>
+      </div>
+    </header>
+
   )
 }
 
