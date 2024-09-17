aa4148876792e35940f02c8046c0abecb83abb28
hannahschwen <hannahschwen@gmail.com>
2024-09-17 19:47:52 -0400

    basic cart item structure

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,6 +1,31 @@
+import '../assets/css/cart.css'
+
 function CartPage() {
   return (
     <>
+      <section>
+        <h2>Shopping cart</h2>
+
+        <div className="cart-item">
+          <figure className="product-img-cart">
+            <img src="images/tempPictures/defaultProductImage.jpg"></img>
+          </figure>
+
+          <div className = "item-text-box">
+            <p>Product Name</p>
+            <p>$5.00</p>
+            <p>Quantity: 1</p>
+          </div>
+
+
+          <button className="btn-del">x</button>
+        </div>
+
+
+        <p>TOTAL: $15.00</p>
+        <button className="btn-1">Check Out</button>
+      </section>
+
     <h2>Cart page</h2>
     </>
   )
