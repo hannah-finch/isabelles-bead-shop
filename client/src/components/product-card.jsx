2a0d12a05835fe8eb3874d50e1c24f5c241448f1
hannahschwen <hannahschwen@gmail.com>
2024-09-17 12:55:46 -0400

    style card except image

--- client/src/components/product-card.jsx
+++ client/src/components/product-card.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom'
 
+import '../assets/css/product-card.css'
 import exampleData from '../assets/example-data.json'
 
 function ProductCard() {
   const Card = (exampleData.products.map((product) => {
     return (
       <Link to={`/product/${product.id}`} key={product.id} data-category={product.category} data-stock={product.quantity}>
-        <div>
-        <p>{product.name}</p>
-        <p>{product.price}</p>
-        <h1>thing</h1>
+        <div className="product-card">
+          <div className="product-image"></div>
+          <p>{product.name}</p>
+          <p>${product.price}</p>
         </div>
       </Link>
     )
