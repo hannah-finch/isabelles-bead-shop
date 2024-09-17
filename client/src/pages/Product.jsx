117530b71d1761bf3a2f468be77f22f6a2318078
hannahschwen <hannahschwen@gmail.com>
2024-09-17 14:10:36 -0400

    get product and render it's info

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,7 +1,27 @@
+import { useParams } from 'react-router-dom'
+ 
+import exampleData from '../assets/example-data.json'
+
 function ProductPage() {
+  // get product id from url
+  const { productId } = useParams();
+  // get product from json by id (get from db later)
+  const [ product ] = exampleData.products.filter( p => p.id === productId )
+  // destructure it
+  const { id, category, description, image, quantity, price, name }  = product
+
   return (
     <>
     <h2>Product page</h2>
+
+    <p>{id}</p>
+    <p>{category}</p>
+    <p>{description}</p>
+    <p>{image.url}</p>
+    <p>{quantity}</p>
+    <p>{price}</p>
+    <p>{name}</p>
+
     </>
   )
 };
