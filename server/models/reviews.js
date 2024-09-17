23b43715e0dd2f11cbdf512b5e795a2dd3dacda2
Daniel Hudson <LuckyLlama72@gmail.com>
2024-09-16 21:20:19 -0400

    Mostly made review schema.

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,1 +1,28 @@
 //TODO
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const reviewSchema = new Schema(
+    {
+        reviewID: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+            maxLength: 300,
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 5,
+        }
+    }
+)
