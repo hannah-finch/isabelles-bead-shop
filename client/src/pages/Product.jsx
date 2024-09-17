4b2ff98f8b908ea16b47f784fb8c9290f0e474fa
hannahschwen <hannahschwen@gmail.com>
2024-09-17 15:11:57 -0400

    structure and style product section
    reviews and the rest of the page are not styled yet

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -14,7 +14,7 @@ function ProductPage() {
     if (quantity < 0) {
       return 'OUT OF STOCK'
     } else {
-      return 'OUT OF STOCK'
+      return
     }
   })
 
@@ -27,23 +27,19 @@ function ProductPage() {
 
         <div className="product-info">
           <h2>{name}</h2>
-          <InStock />
-          <p>Price: <span className="price">${price}</span></p>
+          <p>Price: <span className="price">${price}</span> <InStock /> </p>
+          <p>{description}</p>
+          {/* TODO: Make a dropdown or arrow selection thing to select quantity to add to cart */}
+          <p>Quantity:</p>
+
+          <div className="button-container">
+            <button className="btn-1">Add to Cart</button>
+            <button className="btn-2">Leave a Review</button>
+
+          </div>
 
         </div>
       </section>
-
-
-
-    <h2>{name}</h2>
-
-
-    <p>{category}</p>
-    <p>{description}</p>
-    <p>{image.url}</p>
-
-    <p>{price}</p>
-
 
 {/* I put some product info here just in case we want it to show if admin is logged in */}
     <div>
