98e32e1b99d542d661727b42550d4a88a902b202
hannahschwen <hannahschwen@gmail.com>
2024-09-17 22:56:40 -0400

    form to update product started
    form uses state, need to write switch statement for handleInputChange

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_SINGLE_PRODUCT } from "../utils/queries.js";
@@ -25,6 +26,115 @@ function ProductPage() {
       return;
     }
   };
+
+  // const updateForm = (() => {
+  //   return(
+  //     <h1>Test</h1>
+  //   )
+  // })
+
+  const AdminStuff = () => {
+    const UpdateForm = (() => {
+      const [newName, setName] = useState(name);
+      const [newPrice, setPrice] = useState(price);
+      const [newCategory, setCategory] = useState(category);
+      const [newDescription, setDescription] = useState(description);
+      const [newQuantity, setQuantity] = useState(quantity);
+      const [newImage, setImgae] = useState(image.URL);
+      const [newImageName, setImageName] = useState(image.name);
+      const [newImageDescription, setImageDescription] = useState(image.description);
+
+      const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        // set the state based on input type
+        if (inputType === "name") {
+          setName(inputValue);
+        }
+        if (inputType === "price") {
+          setPrice(inputValue);
+        }
+      };
+    
+      return (
+      <>
+      <h2>Form to edit product</h2>
+        <form onSubmit={""}>
+          <input
+            value={newName}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Product name"
+          ></input>
+          <input
+            value={newPrice}
+            name="price"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Price"
+          ></input>
+          <input
+            value={newQuantity}
+            name="quantity"
+            onChange={handleInputChange}
+            type="number"
+            min="0"
+            placeholder="Stock"
+          ></input>
+          <input
+            value={newCategory}
+            name="category"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Category"
+          ></input>
+          <textarea
+            value={newDescription}
+            name="description"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Description"
+          ></textarea>
+          <input value={image.URL} name="image" type="file"></input>
+          <input
+            value={newImageName}
+            name="category"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Image title"
+          ></input>
+          <input
+            value={newImageDescription}
+            name="imageDescription"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Image caption"
+          ></input>
+
+          <button className="btn-2" type="submit">
+            Submit
+          </button>
+          <br />
+        </form>
+      </>
+      )
+    })
+    return (
+      <section>
+        Number in stock: {quantity} <br></br>
+        Product id: {productId} <br></br>
+        <button className="btn-1" onClick={""}>Edit Product</button>
+        <button className="btn-1" onClick={""}>Delete Product</button>
+
+        <UpdateForm/>
+      </section>
+    )
+  }
+
   if (loading) {
     return <h1>Loading</h1>;
   }
@@ -52,11 +162,12 @@ function ProductPage() {
         </div>
       </section>
 
+      <AdminStuff />
+
       {/* I put some product info here just in case we want it to show if admin is logged in */}
       <div>
         Product info for Admin: <br></br>
-        Number in stock: {quantity} <br></br>
-        Product id: {id}
+
       </div>
     </>
   );
