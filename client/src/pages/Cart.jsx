723c5f814da228d97106c7d38b5403811a429928
hannahschwen <hannahschwen@gmail.com>
2024-09-17 20:04:45 -0400

    basic cart built, no functionality

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,34 +1,46 @@
-import '../assets/css/cart.css'
+import { Link } from "react-router-dom";
+import "../assets/css/cart.css";
 
 function CartPage() {
   return (
     <>
-      <section>
+      <section className="cart-section">
         <h2>Shopping cart</h2>
+
+        {/* TODO: create one of these div's for each item in the cart, using correct data in {} */}
+        <div className="cart-item">
+          <figure className="product-img-cart">
+            <img src="images/tempPictures/defaultProductImage.jpg"></img>
+          </figure>
+          <div className="item-text-box">
+            <Link to="/product/PUTproductIdHERE" className="bold">
+              Product Name
+            </Link>
+            <p>Quantity: 1</p>
+            <p>$5.00 each</p>
+          </div>
+          <button className="btn-del">x</button>
+        </div>
 
         <div className="cart-item">
           <figure className="product-img-cart">
             <img src="images/tempPictures/defaultProductImage.jpg"></img>
           </figure>
-
-          <div className = "item-text-box">
-            <p>Product Name</p>
-            <p>$5.00</p>
-            <p>Quantity: 1</p>
+          <div className="item-text-box">
+            <Link to="/product/PUTproductIdHERE" className="bold">
+              Product Name
+            </Link>
+            <p>Quantity: 2</p>
+            <p>$5.00 each</p>
           </div>
-
-
           <button className="btn-del">x</button>
         </div>
 
-
-        <p>TOTAL: $15.00</p>
+        <p className="total">TOTAL: $15.00</p>
         <button className="btn-1">Check Out</button>
       </section>
-
-    <h2>Cart page</h2>
     </>
-  )
-};
+  );
+}
 
 export default CartPage;
