2ce148e857a8f675677d07b01e798d65f29c49ee
hannahschwen <hannahschwen@gmail.com>
2024-09-19 19:59:03 -0400

    added about text

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,9 +7,7 @@ function AboutPage() {
         <div className="about-text-container">
           <h2>About Me</h2>
           <p>
-            REPLACE THIS TEXT alskdjf laskj dfkljasdk lkj asdfkl jasdklj fklja
-            sdfklj ask jldfij oakjf l;skdfkj askdlj fklj dsfklj askldjklw djflkj
-            awldkfj aklwj dkjl ask djk fjk alsd
+            My name is Isabelle. I have 12 pets and 1 sister. My sister&apos;s name is Kaylee and my 12 pets are Gemma (dog), Riley (dog), Oats (hamster), 3 snails (no names yet), and 6 chickens (King, Lady, Peanut, Shadow, Queen, and Karen). I love making jewelry, painting, and sewing.
           </p>
           <img src="/images/icon-cluster-1.png"></img>
         </div>
@@ -17,11 +15,9 @@ function AboutPage() {
         <div className="img-square-2"></div>
         <div className="about-text-container">
           <img src="/images/icon-cluster-2.png"></img>
-          <h2>About My Shop</h2>
+          <h2>How I Got Started</h2>
           <p>
-            REPLACE THIS TEXT alskdjf laskj dfkljasdk lkj asdfkl jasdklj fklja
-            sdfklj ask jldfij oakjf l;skdfkj askdlj fklj dsfklj askldjklw djflkj
-            awldkfj aklwj dkjl ask djk fjk alsd
+          I have always loved making jewelry, but I don&apos;t really like to wear it. So I just had a bunch of jewelry and didn&apos;t know what to do with it. One day I had an idea, and that idea was to give it to people, and then my mom said that I should sell it. So I decided to sell it to people, but not penguins.
           </p>
         </div>
       </div>
