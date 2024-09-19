28bbdcfeacdd5ab405da756d0519f68c30e4a25e
hannahschwen <hannahschwen@gmail.com>
2024-09-18 20:34:00 -0400

    built and styled about page
    Looks less good on large screens though

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,32 @@
+import "../assets/css/about.css";
+
 function AboutPage() {
   return (
     <>
-    <h2>About page</h2>
+      <div className="about-grid">
+        <div className="about-text-container">
+          <h2>About Me</h2>
+          <p>
+            REPLACE THIS TEXT alskdjf laskj dfkljasdk lkj asdfkl jasdklj fklja
+            sdfklj ask jldfij oakjf l;skdfkj askdlj fklj dsfklj askldjklw djflkj
+            awldkfj aklwj dkjl ask djk fjk alsd
+          </p>
+          <img src="/images/icon-cluster-1.png"></img>
+        </div>
+        <div className="img-square-1"></div>
+        <div className="img-square-2"></div>
+        <div className="about-text-container">
+          <img src="/images/icon-cluster-2.png"></img>
+          <h2>About My Shop</h2>
+          <p>
+            REPLACE THIS TEXT alskdjf laskj dfkljasdk lkj asdfkl jasdklj fklja
+            sdfklj ask jldfij oakjf l;skdfkj askdlj fklj dsfklj askldjklw djflkj
+            awldkfj aklwj dkjl ask djk fjk alsd
+          </p>
+        </div>
+      </div>
     </>
-  )
-};
+  );
+}
 
 export default AboutPage;
