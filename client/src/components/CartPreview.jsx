806e60078acceff930066207945b26a89f9bd47a
swokamoto <swokamoto@gmail.com>
2024-09-22 11:05:38 -0400

    adjusted preview, added img

--- client/src/components/CartPreview.jsx
+++ client/src/components/CartPreview.jsx
@@ -1,23 +1,36 @@
 const CartPreview = ({ items }) => {
-    return (
-      <div className="cart-preview bg-white border border-gray-300 p-4 rounded-lg shadow-lg">
-        {items.length > 0 ? (
-          items.map((item) => (
-            <div key={item._id} className="cart-preview-item mb-4 last:mb-2">
+  return (
+    <div className="cart-preview bg-white border border-gray-300 p-4 rounded-lg shadow-lg">
+      {items.length > 0 ? (
+        items.map((item) => (
+          <div key={item._id} className="cart-preview-item mb-4 last:mb-2 flex">
+            <div className="flex-shrink-0">
+              <img
+                src={item.imageURL}
+                alt={item.name}
+                className="w-12 h-12 object-cover rounded"
+              />
+            </div>
+            <div className="ml-4 flex-1 min-w-0">
               <div className="flex justify-between items-center">
-                <p className="font-semibold whitespace-nowrap">{item.name}</p>
+                <p className="font-semibold truncate">{item.name}</p>
               </div>
               <div className="flex justify-between items-center">
-                <p className="text-sm text-gray-600">Quantity {item.quantity}</p>
-                <p className="text-sm text-gray-600">Total ${(item.price * item.quantity / 100).toFixed(2)}</p>
+                <p className="text-sm text-gray-600">
+                  Quantity {item.quantity}
+                </p>
+                <p className="text-sm text-gray-600">
+                  Total ${((item.price * item.quantity) / 100).toFixed(2)}
+                </p>
               </div>
             </div>
-          ))
-        ) : (
-          <p className="text-center text-gray-600">Your cart is empty</p>
-        )}
-      </div>
-    );
-  };
+          </div>
+        ))
+      ) : (
+        <p className="text-center text-gray-600">Your cart is empty</p>
+      )}
+    </div>
+  );
+};
 
 export default CartPreview;
