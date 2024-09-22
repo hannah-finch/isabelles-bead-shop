92b06f9a5feee9a8b68515e5cdc4166bc0863328
Kalink52 <164951917+Kalink52@users.noreply.github.com>
2024-09-22 09:28:01 -0400

    Update proudct working

--- client/src/components/forms/UpdateForm.jsx
+++ client/src/components/forms/UpdateForm.jsx
@@ -1,39 +1,74 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useMutation } from "@apollo/client";
+import { UPDATE_PRODUCT } from "../../utils/mutations";
 
 function UpdateForm(prop) {
+  const [UpdateProduct] = useMutation(UPDATE_PRODUCT);
   const { description, category, image, name, price, quantity } = prop.product;
+  const { productId } = useParams();
 
   const [formState, setFormState] = useState({
+    id: productId,
     name: name,
     price: price,
     category: category,
     description: description,
     quantity: quantity,
-    image: image.URL,
-    imageName: image.name,
-    imageDescription: image.description,
+    image: image,
   });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+    switch (name) {
+      case "price":
+        setFormState({
+          ...formState,
+          [name]: +value,
+        });
+        break;
+      case "quantity":
+        setFormState({
+          ...formState,
+          [name]: +value,
+        });
+        break;
+      default:
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+    }
   };
 
   //TODO FINISH FUNCTION
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
+    // console.log(formState);
+    // const { name, price, description, image, category, quantity } = formState;
+    try {
+      const UpdatedProduct = await UpdateProduct({
+        variables: formState,
+      });
+
+      console.log(UpdatedProduct);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <>
       <form onSubmit={handleFormSubmit}>
         <h2>Edit product</h2>
+        <label htmlFor="productId">Product ID:(editing disabled)</label>
+        <input
+          value={formState._id}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Product name"
+          disabled
+        ></input>
         <label htmlFor="name">Name:</label>
         <input
           value={formState.name}
@@ -75,28 +110,13 @@ function UpdateForm(prop) {
           type="text"
           placeholder="Description"
         ></textarea>
-        <label htmlFor="image">Image:</label>
+        <label htmlFor="image">Image:(editing disabled)</label>
         <input
           value={formState.image}
           name="image"
           onChange={handleInputChange}
-          type="file"
-        ></input>
-        <label htmlFor="imageName">Image title:</label>
-        <input
-          value={formState.imageName}
-          name="imageName"
-          onChange={handleInputChange}
           type="text"
-          placeholder="Image title"
-        ></input>
-        <label htmlFor="imageDescription">Image Caption:</label>
-        <input
-          value={formState.imageDescription}
-          name="imageDescription"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Image caption"
+          disabled
         ></input>
 
         <div className="button-container">
