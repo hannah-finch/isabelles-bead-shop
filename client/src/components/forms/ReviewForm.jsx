09bf708d65ac1d1db835ee9a14a2b27da673f723
hannahschwen <hannahschwen@gmail.com>
2024-09-22 20:36:25 -0400

    add review works to add to db

--- client/src/components/forms/ReviewForm.jsx
+++ client/src/components/forms/ReviewForm.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_REVIEW } from "../../utils/mutations";
 
 function ReviewForm() {
+  const { productId } = useParams();
+  const [AddReview] = useMutation(ADD_REVIEW);
   const [formState, setFormState] = useState({
     username: "",
     content: "",
@@ -9,49 +14,62 @@ function ReviewForm() {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+    name === "rating"
+      ? setFormState({
+          ...formState,
+          [name]: +value,
+        })
+      : setFormState({
+          ...formState,
+          [name]: value,
+        });
   };
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      await AddReview({
+        variables: { id: productId, reviewDetails: formState },
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <>
-      <form onSubmit={handleFormSubmit}>
+      <form onSubmit={handleFormSubmit} className="narrow">
         <h2>Leave a Review</h2>
+        <label htmlFor="username">Your name:</label>
         <input
           value={formState.username}
           name="username"
           onChange={handleInputChange}
           type="text"
-          placeholder="Your name"
           required
         ></input>
         <label htmlFor="rating">Number of stars:</label>
         <select
           value={formState.rating}
           name="rating"
+          type="number"
           onChange={handleInputChange}
-          placeholder="Star rating (1-5)"
           required
         >
+          <option value=""></option>
           <option value="5">5</option>
           <option value="4">4</option>
           <option value="3">3</option>
           <option value="2">2</option>
           <option value="1">1</option>
         </select>
+        <label htmlFor="content">Review text:</label>
         <textarea
           value={formState.content}
           name="content"
           onChange={handleInputChange}
           type="text"
-          placeholder="Review text"
           required
         ></textarea>
         <button className="btn-1" type="submit">
