81bde5124c9c207aa2abab589347b1b7f92867e0
swokamoto <swokamoto@gmail.com>
2024-09-25 09:51:23 -0400

    final checks, all good.  just needs StrictMode OFF when we go live

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ShoppingCart from "../components/ShoppingCart";
 import CheckoutButton from "../components/CheckoutButton";
 import { ShoppingCartContext } from "../utils/ProductsContext";
@@ -6,11 +6,13 @@ import { ShoppingCartContext } from "../utils/ProductsContext";
 export default function Cart() {
   const { cartItems, setCartItems } = useContext(ShoppingCartContext);
 
-  // check local storage for cart items
-  const savedcartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
-  if (savedcartItems.length > 0 && cartItems.length === 0) {
-    setCartItems(savedcartItems);
-  }
+  // check local storage for cart items. useEffect runs only once when the component mounts
+  useEffect(() => {
+    const savedcartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    if (savedcartItems.length > 0 && cartItems.length === 0) {
+      setCartItems(savedcartItems);
+    }
+  }, []);
 
   return (
     <>
