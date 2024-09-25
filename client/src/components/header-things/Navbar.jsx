ac81d119532c989048bf8d4cef20c62eaebaba49
hannahschwen <hannahschwen@gmail.com>
2024-09-25 17:20:56 -0400

    built and styled hamburger menu

--- client/src/components/header-things/Navbar.jsx
+++ client/src/components/header-things/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { ShoppingCartContext } from "../../utils/ProductsContext";
 import CartPreview from "../CartPreview";
 
@@ -8,71 +8,135 @@ import Auth from "../../utils/auth";
 function Navbar() {
   const { cartCounter, cartItems } = useContext(ShoppingCartContext);
   const [isHovered, setIsHovered] = useState(false);
-  return (
-    <nav>
-      {Auth.isLoggedIn() ? (
-        Auth.isAdmin() ? (
-          <>
-            <NavLink
-              to={"/admin"}
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
-              Admin Panel
-            </NavLink>
-            <div className="vertical-line"></div>
-          </>
-        ) : null
-      ) : null}
-      <NavLink
-        to={"/"}
-        className={({ isActive }) => (isActive ? "active-link" : "")}
-      >
-        shop
-      </NavLink>
-      <NavLink
-        to={"/about"}
-        className={({ isActive }) => (isActive ? "active-link" : "")}
-      >
-        about
-      </NavLink>
+  const [showMenu, setShowMenu] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
 
-      {Auth.isLoggedIn() ? (
-        Auth.isClient() ? (
-          <NavLink
-            to={"/account/userId"}
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            account
-          </NavLink>
-        ) : null
-      ) : null}
+  const menu = useRef(null);
 
-      {Auth.isLoggedIn() ? (
-        <button onClick={Auth.logout}>logout</button>
-      ) : (
+  // get the view width to show/hide navbar
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // click off menu to close it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menu.current && !menu.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const NavLinks = () => {
+    return (
+      <>
+        {Auth.isLoggedIn() ? (
+          Auth.isAdmin() ? (
+            <>
+              <NavLink
+                to={"/admin"}
+                className={({ isActive }) =>
+                  isActive ? "active-link btn-admin" : "btn-admin"
+                }
+              >
+                Admin Panel
+              </NavLink>
+              <div className="vertical-line"></div>
+            </>
+          ) : null
+        ) : null}
         <NavLink
-          to={"/login"}
+          to={"/"}
           className={({ isActive }) => (isActive ? "active-link" : "")}
         >
-          login
+          shop
         </NavLink>
-      )}
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          about
+        </NavLink>
 
-      <div className="vertical-line"></div>
+        {Auth.isLoggedIn() ? (
+          Auth.isClient() ? (
+            <NavLink
+              to={"/account/userId"}
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              account
+            </NavLink>
+          ) : null
+        ) : null}
 
-      <NavLink
-        to={"/cart"}
-        className={({ isActive }) =>
-          `${isActive ? "active-link" : ""} nav-link`
-        }
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        cart
-        <div className="cart-num">{cartCounter}</div>
-        {isHovered && <CartPreview items={cartItems} />}
-      </NavLink>
-    </nav>
+        {Auth.isLoggedIn() ? (
+          <button onClick={Auth.logout}>logout</button>
+        ) : (
+          <NavLink
+            to={"/login"}
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            login
+          </NavLink>
+        )}
+        <div className="vertical-line"></div>
+      </>
+    );
+  };
+  return (
+    <>
+      <nav>
+        {showMenu && (
+          <div className="menu" ref={menu}>
+            <button className="hamburger" onClick={toggleMenu}>
+              =
+            </button>
+            <div className="spacer"></div>
+            <NavLinks />
+
+            <img style={{width: "60px", alignSelf: "center",  position: "absolute", bottom: "60px",}}src="/images/icons/cluster-1.svg"></img>
+          </div>
+        )}
+        <button className="hamburger" onClick={toggleMenu}>
+          =
+        </button>
+
+        {width > 700 ? <NavLinks /> : <></>}
+
+        <NavLink
+          to={"/cart"}
+          className={({ isActive }) =>
+            `${isActive ? "active-link" : ""} nav-link`
+          }
+          onMouseEnter={
+            width > 700 ? () => setIsHovered(true) : () => setIsHovered(false)
+          }
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          cart
+          <div className="cart-num">{cartCounter}</div>
+          {isHovered && <CartPreview items={cartItems} />}
+        </NavLink>
+      </nav>
+      {/* <div className="menu">
+        <NavLinks />
+      </div> */}
+    </>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,7 @@ import "./assets/css/product-card.css"
 import "./assets/css/admin.css"
 import "./assets/css/about.css"
 import "./assets/css/cart.css"
+import "./assets/css/queries.css"
 
 //Importing pages
 import HomePage from "./pages/Home.jsx";
